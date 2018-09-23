8e55e0a439209ec626d0c4eb8bff12585b61be35
buingochai <haibuai3@gmail.com>
2018-09-24 06:22:39 +0700

    Hit

--- Buoi_1/LopJava/src/Basic/bai_6.java
+++ Buoi_1/LopJava/src/Basic/bai_6.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Basic;
 
-/**
- *
- * @author Vicker
- */
+
 import java.util.Scanner;
 public class bai_6 {
     public static void main(String[] args){
